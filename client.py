250a1f84a5a321dd0850e7a88b9556e31db9c8dc
motviybletb <98744794+motviybletb@users.noreply.github.com>
2024-06-25 18:32:48 +0300

    client.py

--- client.py
+++ client.py
@@ -1,9 +1,22 @@
 import socket
 
 
-def send_image(image_path):
+def is_binary_data(data):
+    return data.startswith((b'\x89PNG', b'\xFF\xD8\xFF', b'GIF87a', b'GIF89a'))
+
+
+def send_image(image_path, prefix_byte='AA'):
     with open(image_path, 'rb') as img_file:
         img_bytes = img_file.read()
+
+    if prefix_byte.upper() == 'AA':
+        extra_byte = b'\xAA'
+    elif prefix_byte.upper() == 'AB':
+        extra_byte = b'\xAB'
+    else:
+        raise ValueError("prefix_byte must be either 'AA' or 'AB'")
+
+    img_bytes = extra_byte + img_bytes
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 50000))
@@ -11,10 +24,26 @@ def send_image(image_path):
     client_socket.sendall(img_bytes)
     client_socket.shutdown(socket.SHUT_WR)
 
-    response = client_socket.recv(1024)
-    print("Received:", response.decode())
+    response = b''
+    while True:
+        chunk = client_socket.recv(4096)
+        if not chunk:
+            break
+        response += chunk
+
     client_socket.close()
+
+    if is_binary_data(response):
+        with open('received_mask.png', 'wb') as f:
+            f.write(response)
+        print("Received mask image saved as 'received_mask.png'")
+    else:
+        try:
+            print("Received:", response.decode('utf-8'))
+        except UnicodeDecodeError:
+            print("Received data could not be decoded as UTF-8. Raw data:", response)
 
 
 if __name__ == "__main__":
-    send_image('microscope_image_112.png')
+    send_image('microscope_image_112.png', prefix_byte='AB')
+
